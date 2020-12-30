7b8f6849a4c5359f0bced2200ee0acbe11378296
mohdimranm <immohdimran8@gmail.com>
2020-12-30 21:30:01 +0530

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const btnclose = document.querySelector('.close-modal');
+
+const btnopen = document.querySelectorAll('.show-modal');
+
+console.log(btnopen);
+
+const openmodal = function () {
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+const closemodel = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnopen.length; i++) {
+    btnopen[i].addEventListener('click', openmodal);
+}
+
+
+btnclose.addEventListener('click', closemodel);
+overlay.addEventListener('click', closemodel);
